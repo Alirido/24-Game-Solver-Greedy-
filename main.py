f9efd4b8cc9f0cb86c2f88bb3b842f753bd8f045
Ali Rido <mohalirido@gmail.com>
2019-02-10 21:08:09 +0700

    Update main program

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 '''
 Hello guys, jadi ini program saya untuk memecahkan 24 game solver.
 Saya menggunakan strategi algoritma greedy
-
-Perintah:
-q = Quit
-i = Input
 '''
 
 def welcome():
 	print(__doc__)
 	print("Enter your input file: ")
-	print("Untuk sementara input manual dulu aja disini: ")
+	print("Untuk sementara input manual dulu aja disini:")
+
+def docum():
+	print("\nList of commands: ")
+	print("\tq = Quit")
+	print("\ti = Input")
+	print("\td = Documentation\n")
 
 def main():
 	welcome()
+	docum()
 	c = input("Enter command: ")
 	while not (c.lower() == 'q'):
-		print("Your input key is %c" % c)
+		if (c.lower() == 'i'):
+			print("Your input key is %s" % c)
+		elif c.lower() == 'd':
+			docum()
+		else:
+			print("%s is invalid input." % c)
 		c = input("Enter command: ")
 	print("Thank you and see you later:)")
 
