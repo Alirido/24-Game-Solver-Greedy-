c4b08e288ff8277a30f3c39c24dc0fb20f3d7788
Ali Rido <mohalirido@gmail.com>
2019-02-10 22:47:36 +0700

    Add open file external to main program

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Hello guys, jadi ini program saya untuk memecahkan 24 game solver.
 Saya menggunakan strategi algoritma greedy
 '''
+from os import path as p
 
 def welcome():
 	print(__doc__)
@@ -14,13 +15,26 @@ def docum():
 	print("\ti = Input")
 	print("\td = Documentation\n")
 
+def readFile():
+	filename = input("Enter your input filename: ")
+	while not p.exists(filename):
+		print(filename, "file does not exists")
+		filename = input("Enter your input filename: ")
+	f = open(filename)
+	data = f.readline()
+	digits = data.split()
+	return digits
+
 def main():
 	welcome()
 	docum()
 	c = input("Enter command: ")
 	while not (c.lower() == 'q'):
 		if (c.lower() == 'i'):
-			print("Your input key is %s" % c)
+			digits = readFile()
+			for num in digits:
+				print(num)
+			print()
 		elif c.lower() == 'd':
 			docum()
 		else:
