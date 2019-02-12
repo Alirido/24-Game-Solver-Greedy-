700550d914731c3f1c4b5554bc3aae4df87b8b02
Ali Rido <mohalirido@gmail.com>
2019-02-12 11:26:11 +0700

    Update main program

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ Hello guys, jadi ini program saya untuk memecahkan 24 game solver.
 Saya menggunakan strategi algoritma greedy
 '''
 from os import path as p
-import operator
 
 def welcome():
 	print(__doc__)
@@ -14,7 +13,8 @@ def docum():
 	print("\nList of commands: ")
 	print("\tq = Quit")
 	print("\ti = Input")
-	print("\td = Documentation\n")
+	print("\td = Documentation")
+	print("\tt = Testing\n")
 
 def readFile():
 	filename = input("Enter your input filename: ")
@@ -27,7 +27,7 @@ def readFile():
 	f.close()
 	return digits
 
-def ito(x):
+def ito(x): # ito = Integer to Operator
 	if (x==5):
 		return '+'
 	elif (x==4):
@@ -37,36 +37,29 @@ def ito(x):
 	else:
 		return '/'
 
-def get_operator_fn(op):
-	return {
-	'+' : operator.add,
-	'-' : operator.sub,
-	'*' : operator.mul,
-	'/' : operator.div,
-	'%' : operator.mod,
-	'^' : operator.xor,
-	}[op]
-
-def eval_binary_expr(op1, oper1, op2, oper2, op3, oper3, op4):
-	op1,op2,op3,op4 = int(op1), int(op2), int(op3), int(op4)
-	return get_operator_fn(oper)(op1, op2)
-
 def solve(arr): # USING GREEDY ALGORITHM
-	sum = 15
-	found = False
-	while (sum>=6 and not found):
+	score = 15
+	found24 = False
+	maks_score = -10000
+	while (score>=6 and not found24):
 		# Generate all possible permutation of digits
 		a = 5
-		while (a>=2 and not found):
+		while (a>=2 and not found24):
 			b = 5
-			while (b>=2 and not found):
+			while (b>=2 and not found24):
 				c = 5
-				while (c>=2 and not found):
-					ta = ito(a)
-					tb = ito(b)
-					tc = ito(c)
-					return eval('arr[0] ta arr[1] tb arr[2] tc arr[3]')
-		sum -= 1
+				while (c>=2 and not found24):
+					if (a+b+c > score):
+						continue
+					elif (a+b+c == score):
+						oa = ito(a)
+						ob = ito(b)
+						oc = ito(c)
+						ans = "%d %c %d %c %d %c %d"
+						ans = ans % (arr[0]. oa, arr[1], ob, arr[2], oc, arr[3])
+						# if (eval(ans))
+		score -= 1
+					# return eval('arr[0] ta arr[1] tb arr[2] tc arr[3]')
 
 def main():
 	welcome()
@@ -76,16 +69,20 @@ def main():
 		if (c.lower() == 'i'):
 			digits = readFile()
 			print("Jawabannya adalah: ")
-			t1 = ito(5)
-			t2 = ito(4)
-			t3 = ito(3)
-			print(eval_binary_expr("10 t1 20 t2 30 t3 40".split()))
-			# print(solve(digits))
-			# for num in digits:
-			# 	print(num)
+			for num in digits:
+				print(num)
 			print()
 		elif c.lower() == 'd':
 			docum()
+		elif c.lower() == 't':
+			# FOR TESTING
+			brackets = ( [()] + [(x,y)
+                         for x in range(0, 7, 2)
+                         for y in range(x+4, 7+2, 2)
+                         if (x,y) != (0,7+1)]
+                 + [(0, 3+1, 4+2, 7+3)] ) # double brackets case
+			print("Brackets = ", end="")
+			print(brackets)
 		else:
 			print("%s is invalid input." % c)
 		c = input("Enter command: ")
---
+++ tes.py
@@ -0,0 +1,4 @@
+def main():
+	ta = '+'
+	buffer = "%d %s %d %s %d" % (a, ta)
+	buffer = 
